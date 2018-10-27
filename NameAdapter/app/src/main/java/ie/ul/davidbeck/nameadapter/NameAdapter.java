02af5dea53a7298e9515c9dd25473e5b3dc96a92
David-Beck-UL <39083800+David-Beck-UL@users.noreply.github.com>
2018-10-27 22:33:43 +0100

    Animate insert and scroll to top

--- NameAdapter/app/src/main/java/ie/ul/davidbeck/nameadapter/NameAdapter.java
+++ NameAdapter/app/src/main/java/ie/ul/davidbeck/nameadapter/NameAdapter.java
@@ -18,10 +18,21 @@ public class NameAdapter extends RecyclerView.Adapter<NameAdapter.NameViewHolder
 
     private List<String> mNames = new ArrayList<>();
     private Random mRandom = new Random();
+    private RecyclerView mRecyclerView;
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        mRecyclerView = recyclerView;
+    }
 
     public void addName(){
         mNames.add(0, getRandomName());
-        notifyDataSetChanged();
+        //notifyDataSetChanged();
+        notifyItemInserted(0);
+        notifyItemRangeChanged(0, mNames.size());
+        mRecyclerView.scrollToPosition(0);
+
     }
 
     private String getRandomName() {
